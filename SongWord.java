56d741736ddf6e62297383b17bf2bd7b1d227cc4
Lauren Pak <>
2021-04-28 15:07:00 +0000

    Lauren Pak - Unit 7 Q2

--- SongWord.java
+++ SongWord.java
@@ -1,9 +1,21 @@
+//Name: Lauren Pak
+//Period: 1
+//Date: 4/27/21
 public class SongWord
 {
   public int findCount(String[][]songArray, String word)
   {
-
-
-    return 0;
+    int total = 0;
+    for(int r = 0; r < songArray.length; r++)
+    {
+      for(int c = 0; c < songArray[0].length; c++)
+      {
+        if(songArray[r][c].indexOf(word) != -1)
+        {
+          total++;
+          }
+          }
+    }
+return total;
   }
 }
